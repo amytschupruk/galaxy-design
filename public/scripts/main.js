2f7065647ae7f35d8744a4b935820e0108656be4
Amy Tschupruk <amytschupruk@gmail.com>
2017-07-01 19:01:24 -0400

    styles and plugins for first three sections

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,21 @@
-"use strict";
+'use strict';
+
+// Isotope grid
+var $grid = $('.portfolio--gallery__container').isotope({
+  // customized options...
+  itemSelector: '.portfolio--gallery__cell',
+  percentPosition: true,
+  masonry: {
+    // use element for option
+    columnWidth: '.grid-sizer'
+  }
+});
+// layout Isotope after each image loads
+$grid.imagesLoaded().progress(function () {
+  $grid.isotope('layout');
+  //isotope filter functions
+  $('#filters').on('click', 'button', function () {
+    var filterValue = $(this).attr('data-filter-value');
+    $grid.isotope({ filter: filterValue });
+  });
+});
