29d75ab04599d8c49700c90cf7b7cafa82a47613
Amy Tschupruk <amytschupruk@gmail.com>
2017-07-07 15:16:55 -0400

    smooth scroll added

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -10,6 +10,25 @@ var $grid = $('.portfolio--gallery__container').isotope({
     columnWidth: '.grid-sizer'
   }
 });
+
+$("a").on('click', function (event) {
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+    // Store hash
+    var hash = this.hash;
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function () {
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+  } // End if
+});
+
 // layout Isotope after each image loads
 $grid.imagesLoaded().progress(function () {
   $grid.isotope('layout');
