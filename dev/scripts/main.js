cb3c1ca387d05e5aa89eeaabd7627544daac7add
Amy Tschupruk <amytschupruk@gmail.com>
2017-07-08 19:46:47 -0400

    added responsive features to sections, still need to add responsive nav

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,58 +1,66 @@
-
-// Isotope grid
-var $grid = $('.portfolio--gallery__container').isotope({
-  // customized options...
-  itemSelector: '.portfolio--gallery__cell',
-  percentPosition: true,
-  masonry: {
-    // use element for option
-    columnWidth: '.grid-sizer'
-  }
-});
-
-$("a").on('click', function (event) {
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
-    // Prevent default anchor click behavior
-    event.preventDefault();
-    // Store hash
-    var hash = this.hash;
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function () {
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
+$(document).ready(function(){
+    // Isotope grid
+    var $grid = $('.portfolio--gallery__container').isotope({
+      // customized options...
+      itemSelector: '.portfolio--gallery__cell',
+      percentPosition: true,
+      masonry: {
+        // use element for option
+        columnWidth: '.grid-sizer'
+      }
     });
-  } // End if
-});
 
-// layout Isotope after each image loads
-$grid.imagesLoaded().progress( function() {
-  	$grid.isotope('layout');
-  	//isotope filter functions
-	$('#filters').on( 'click', 'button', function() {
-	  var filterValue = $(this).attr('data-filter-value');
-	  $grid.isotope({ filter: filterValue });
-	});
-});
+    //Smooth Scroll Plugin
+    $("a").on('click', function (event) {
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+        // Store hash
+        var hash = this.hash;
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
 
-$("a.single_image").fancybox({
-});
+    //Masonry Isotope plugin
+    // layout Isotope after each image loads
+    $grid.imagesLoaded().progress( function() {
+      	$grid.isotope('layout');
+      	//isotope filter functions
+    	$('#filters').on( 'click', 'button', function() {
+    	  var filterValue = $(this).attr('data-filter-value');
+    	  $grid.isotope({ filter: filterValue });
+      });
+    });
+    //default isotope filter to be on "ads"
+    $('.portfolio--gallery__container').isotope({ filter: '.ad' });
 
-$('.main-carousel').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true
-});
+    //Fancybox plugin to view individual images
+    $("a.single_image").fancybox({
+    });
 
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 550) {
-      $('nav').addClass('sticky-top');
-    } else {
-      $('nav').removeClass('sticky-top');
-    }
+    //flickity plugin for Clients Section
+    $('.main-carousel').flickity({
+      // options
+      cellAlign: 'left',
+      contain: true
+    });
+
+    //Sticky nav jQuery
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 550) {
+          $('nav').addClass('sticky-top');
+        } else {
+          $('nav').removeClass('sticky-top');
+        }
+    });
 });
 
 
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,56 +1,65 @@
 'use strict';
 
-// Isotope grid
-var $grid = $('.portfolio--gallery__container').isotope({
-  // customized options...
-  itemSelector: '.portfolio--gallery__cell',
-  percentPosition: true,
-  masonry: {
-    // use element for option
-    columnWidth: '.grid-sizer'
-  }
-});
-
-$("a").on('click', function (event) {
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
-    // Prevent default anchor click behavior
-    event.preventDefault();
-    // Store hash
-    var hash = this.hash;
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function () {
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-    });
-  } // End if
-});
-
-// layout Isotope after each image loads
-$grid.imagesLoaded().progress(function () {
-  $grid.isotope('layout');
-  //isotope filter functions
-  $('#filters').on('click', 'button', function () {
-    var filterValue = $(this).attr('data-filter-value');
-    $grid.isotope({ filter: filterValue });
+$(document).ready(function () {
+  // Isotope grid
+  var $grid = $('.portfolio--gallery__container').isotope({
+    // customized options...
+    itemSelector: '.portfolio--gallery__cell',
+    percentPosition: true,
+    masonry: {
+      // use element for option
+      columnWidth: '.grid-sizer'
+    }
   });
-});
 
-$("a.single_image").fancybox({});
+  //Smooth Scroll Plugin
+  $("a").on('click', function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 
-$('.main-carousel').flickity({
-  // options
-  cellAlign: 'left',
-  contain: true
-});
+  //Masonry Isotope plugin
+  // layout Isotope after each image loads
+  $grid.imagesLoaded().progress(function () {
+    $grid.isotope('layout');
+    //isotope filter functions
+    $('#filters').on('click', 'button', function () {
+      var filterValue = $(this).attr('data-filter-value');
+      $grid.isotope({ filter: filterValue });
+    });
+  });
+  //default isotope filter to be on "ads"
+  $('.portfolio--gallery__container').isotope({ filter: '.ad' });
 
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 550) {
-    $('nav').addClass('sticky-top');
-  } else {
-    $('nav').removeClass('sticky-top');
-  }
+  //Fancybox plugin to view individual images
+  $("a.single_image").fancybox({});
+
+  //flickity plugin for Clients Section
+  $('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
+
+  //Sticky nav jQuery
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 550) {
+      $('nav').addClass('sticky-top');
+    } else {
+      $('nav').removeClass('sticky-top');
+    }
+  });
 });
